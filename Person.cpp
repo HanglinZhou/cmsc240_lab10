112c44038401f2b1629bacc7433a045a2d2e6b2e
hanglinzhou <hanglin.zhou@richmond.edu>
2018-04-02 14:04:42 -0400

    Add instance variables to Person.h and create Person.cpp.

---
+++ Person.cpp
@@ -0,0 +1,67 @@
+#ifndef __PERSON_CPP__
+#define __PERSON_CPP__
+
+#include "Person.h"
+#include <iostream>
+
+Person::Person(){
+
+}
+
+Person::Person(const Person& other){
+
+}
+
+Person::Person(int urid, std::string netid, std::string lname, std::string
+               fname, int dob_day, int dob_month, int dob_year, std::string
+               email, std::string address, long phone){
+
+}
+
+
+Person::~Person(){
+
+}
+
+std::string Person::getFirstName(){
+
+}
+
+std::string Person::getLastName(){
+
+}
+
+struct tm Person::getDateOfBirth(){
+
+}
+
+std::string Person::getAddress(){
+
+}
+
+std::string Person::getNetID(){
+
+}
+
+int
+
+        std::string  getLastName();
+        struct tm    getDateOfBirth();
+            // see http://www.cplusplus.com/reference/ctime/tm/
+        std::string  getAddress();
+        std::string  getNetID();
+        int          getURID();
+        std::string  getEmail();
+        long         getPhone();
+
+        void setFirstName(std::string fname);
+        void setLastName(std::string lname);
+        void setDateOfBirth(int day, int month, int year);
+        void setAddress(std::string address);
+        void setNetID(std::string netid);
+        void setURID(int urid);
+        void setEmail(std::string email);
+        void setPhone(long number);
+};
+
+#endif
