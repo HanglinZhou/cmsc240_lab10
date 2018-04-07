3b4f7663feab6a6135e51578a489aa833b11d6f2
hanglinzhou <hanglin.zhou@richmond.edu>
2018-04-06 21:45:56 -0400

    Implemented most of Person.cpp and tested getter/setters and one constructor.

--- Person.cpp
+++ Person.cpp
@@ -15,7 +15,16 @@ Person::Person(const Person& other){
 Person::Person(int urid, std::string netid, std::string lname, std::string
                fname, int dob_day, int dob_month, int dob_year, std::string
                email, std::string address, long phone){
-
+  URID = urid;
+  NetID = netid;
+  lastName =lname;
+  firstName = fname;
+  dob.tm_mday = dob_day;
+  dob.tm_mon = dob_month - 1;
+  dob.tm_year = dob_year - 1900;
+  this->email = email;
+  this->address = address;
+  this->phone = phone;
 }
 
 
@@ -24,60 +33,70 @@ Person::~Person(){
 }
 
 std::string Person::getFirstName(){
-  return "first name?";
+  return firstName;
 }
 
 std::string Person::getLastName(){
-  return "last name?";
+  return lastName;
 }
 
 struct tm Person::getDateOfBirth(){
-  struct tm wrongDOB;
-  return wrongDOB;
+  return dob;
 }
 
 std::string Person::getAddress(){
-  return "address?";
+  return address;
 }
 
 std::string Person::getNetID(){
-  return "netID?";
+  return NetID;
 }
 
 int Person::getURID(){
-  return 0;
+  return URID;
 }
 
 std::string Person::getEmail(){
-  return "email?";
+  return email;
 }
 
 long Person::getPhone(){
-  return 000000000;
+  return phone;
 }
 
 void Person::setFirstName(std::string fname){
+  firstName = fname;
 }
 
 void Person::setLastName(std::string lname){
+  lastName = lname;
 }
 
 void Person::setDateOfBirth(int day, int month, int year){
+  dob.tm_mday = day;
+  dob.tm_mon = month - 1;
+  dob.tm_year = year - 1900;
 }
 
 void Person::setAddress(std::string address){
+  this->address = address;
 }
 
 void Person::setNetID(std::string netid){
+  NetID = netid;
 }
 
 void Person::setURID(int urid){
+  URID = urid;
 }
 
 void Person::setEmail(std::string email){
+  this->email = email;
 }
 
+
 void Person::setPhone(long number){
+  phone = number;
 }
 
 
--- personTester.cpp
+++ personTester.cpp
@@ -14,5 +14,57 @@ int main(){
   cout << "p.setFirstName(\"Harry\")" << endl;
   p.setFirstName("Harry");
   cout << "First name: " << p.getFirstName() << "[Expected: Harry]" << endl;
+
+  cout << "p.setLastName(\"Potter\")" << endl;
+  p.setLastName("Potter");
+  cout << "Last name: " << p.getLastName() << "[Expected: Potter]" << endl;
+
+  cout << "p.setDateOfBirth(31, 7, 1980)" << endl;
+  p.setDateOfBirth(31, 7, 1980);
+  cout << "Date of birth: " << p.getDateOfBirth().tm_mon + 1 << "/" <<
+  p.getDateOfBirth().tm_mday << "/" << p.getDateOfBirth().tm_year + 1900<<
+  "[Expected: 7/31/1980]" << endl;
+
+  cout << "p.setAddress(\"4 Private Dr\")" << endl;
+  p.setAddress("4 Private Dr");
+  cout << "Address: " << p.getAddress() << "[Expected: 4 Private Dr]" << endl;
+
+  cout << "p.setNetID(\"hp2bz\")" << endl;
+  p.setNetID("hp2bz");
+  cout << "NetID: " << p.getNetID() << "[Expected: hp2bz]" << endl;
+
+  cout << "p.setURID(10000000)" << endl;
+  p.setURID(10000000);
+  cout << "URID: " << p.getURID() << "[Expected: 10000000]" << endl;
+
+  cout << "p.setEmail(\"harry.potter@hogwarts.ac.uk\")" << endl;
+  p.setEmail("harry.potter@hogwarts.ac.uk");
+  cout << "Email: " << p.getEmail() << "[Expected: harry.potter@hogwarts.ac.uk]" << endl;
+
+  cout << "p.setPhone(111111)" << endl;
+  p.setPhone(111111);
+  cout << "Phone: " << p.getPhone() << "[Expected: 111111]" << endl;
+
+  //test constructors
+  cout << "Testing constructors:" << endl;
+  cout << "-------------------------------------" << endl << endl;
+
+  cout << "Person p2 (20000000, \"sb2bz\", \"B \"sirius.black@hogwarts.ac.uk\", \"12 Grimmauld Place\", 22222)" << endl;
+  Person p2 (20000000, "sb2bz", "Black", "Sirius", 3, 11, 1959, "sirius.black@hogwarts.ac.uk", "12 Grimmauld Place", 22222);
+  cout << "First name: " << p2.getFirstName() << "[Expected: Sirius]" << endl;
+  cout << "Last name: " << p2.getLastName() << "[Expected: Black]" << endl;
+  cout << "Date of birth: " << p2.getDateOfBirth().tm_mon + 1<< "/" <<
+  p2.getDateOfBirth().tm_mday << "/" << p2.getDateOfBirth().tm_year + 1900<<
+  "[Expected: 11/3/1959]" << endl;
+  cout << "Address: " << p2.getAddress() << "[Expected: 12 Grimmauld Place]" << endl;
+  cout << "NetID: " << p2.getNetID() << "[Expected: sb2bz]" << endl;
+  cout << "URID: " << p2.getURID() << "[Expected: 20000000" << endl;
+  cout << "Email: " << p2.getEmail() << "[Expected:sirius.black@hogwarts.ac.uk]" << endl;
+  cout << "Phone: " << p2.getPhone() << "[Expected: 22222]" << endl;
+
+
+
+
+
   return (0);
 }
